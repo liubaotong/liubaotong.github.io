df89d32f4bc6c8eef019733b29ca35fd12fe4ac3
baotongliu <baotongliut@qq.com>
2023-03-24 14:55:54 +0800

    添加淮南子

--- jingdian/python.py
+++ jingdian/python.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 
-for i in range(1, 82):
-    name = 'qihuang/lingshu/{}.txt'.format(i)
-    f = open(name, 'w')
-    f.write('空白')
-    f.close()
-
-    name = 'qihuang/nanjing/{}.txt'.format(i)
-    f = open(name, 'w')
-    f.write('空白')
-    f.close()
-
-    name = 'qihuang/suwen/{}.txt'.format(i)
+for i in range(1, 22):
+    name = 'dao/honglie/{}.txt'.format(i)
     f = open(name, 'w')
     f.write('空白')
     f.close()

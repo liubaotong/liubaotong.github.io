3bb572129d6c5215db8fc303bc5e286ac1e75a4e
baotongliu <baotongliut@qq.com>
2023-03-25 12:27:34 +0800

    整理三十六计

--- jingdian/python.py
+++ jingdian/python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-for i in range(1, 22):
-    name = 'dao/honglie/{}.txt'.format(i)
+for i in range(22, 37):
+    name = 'bing/sanshiliuji/{}.txt'.format(i)
     f = open(name, 'w')
     f.write('空白')
     f.close()
